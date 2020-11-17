b12129292c2e805f264363fa86bc2c25626a8c22
HaruhKi <mik.vanrgby@gmail.com>
2020-11-18 00:37:10 +0900

    add comment and replace address

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
-
 import sys
 import pandas as pd
 import smtplib
-from email.mime.text import MIMEText
 import datetime
 import http.server
 import socketserver
 import sendMail 
+import re
+from email.mime.text import MIMEText
+from collections import Counter
 
-
-
-
-# 不具合一覧
-# INOPERATION 点火
-# WORN OUT
-# SUPPLY
-# DISCHARGER 放電
-# LIGHTNING 落雷
-# BIRD バードストライク
-# RECLINING 取り替え
-
-TROUBLE = ["INOPERATION","WORN","SUPPLY","DISCHARGER","LIGHTNING","BIRD","RECLINING","STATIC"]
-# TROUBLE = input()
+# 機体番号一覧
 MACHINE_NUMBER = ["01RK","02RK","03RK","04RK","05RK","06RK"]
+# REMOVE_WORDは検索から外したい単語一覧です。右に単語を追加していくことが出来ます。
+REMOVE_WORD = ["-","OUT","OUT.","ON","IN","AT","OF","NG","RH","LH","MAIN","FOUND"]
 
+# ここにExcelファイルからexportしたcsvファイルの名前を記載します。
+# csvファイルはこのファイル（main.py）と同じ場所に置きます。
 file = pd.read_csv('data.csv')
+
 file = file.dropna(how = "all",axis=1)
 file = file.dropna(how = "all")
-# print("不具合情報:")
-# value = input()
-# value = value.upper()
-d = datetime.datetime.today()
-date = d.strftime("%Y-%m-%d")
+
+inpe = file['不具合情報']
+inpe_count = len(inpe)
+
+all_list = []
+for i in range(inpe_count):
+    ilist = inpe[i].split()
+    alist  = [ilist[i] for i in range(len(ilist))]
+    all_list += alist
+c = Counter(all_list)
+
+
+re_comp = re.compile("^NO.[0-9].*$")
+count_over = [i[0] for i in c.items() if i[1] >= 3]
+remove_no  = [count_over[i] for i in range(len(count_over)) if not re_comp.match(count_over[i])]
+TROUBLE = [remove_no[i] for i in range(len(remove_no)) if remove_no[i] not in REMOVE_WORD]
+
+# 下二行のコメントを外すと検索がかかっている不具合一覧が見れます。
+# 検索に単語を追加したい場合は「TROUBLE.append("ここに検索したい単語を入れる")」のコメントアウトを削除してください。
+
+# print("↓不具合一覧↓\n")
+# print(TROUBLE)
+
+# TROUBLE.append("ここに検索したい単語を入れる")
 
 
 def trouble_search (file, number):
@@ -41,24 +53,18 @@ def trouble_search (file, number):
         inpe_count = len(inpe)
         if (inpe_count >= 3):
             inpe.to_csv('to_csv_out_' + number + '_' + key + '.csv')
-            trouble_data = trouble_data + number + "の" + key + "の不具合は3回以上です。" + '\n'
+            trouble_data = trouble_data + key + "の不具合は" + str(inpe_count) + "回です。\n"
+    print("【機体番号　" + number + "　】\n")
     print(trouble_data)
     return inpe_count,trouble_data
-    #return に trouble_data　を追加しました
+
 
 mailContent = ''
 for key in MACHINE_NUMBER:
     inpe = file[file['機番'].str.match('^.*' + key + '.*$')]
-    # inpe.to_csv('to_csv_out_' + key + '_' + date + '.csv')
     inpe_count,trouble_data =  trouble_search(inpe,key)
-    mailContent = mailContent + trouble_data
+    mailContent = mailContent + "【機体番号　" + key + "　】\n" + trouble_data + "\n"
     
 sendMail.send(mailContent)
 
-# PORT = 8000
-# Handler = http.server.SimpleHTTPRequestHandler
-
-# with socketserver.TCPServer(("", PORT), Handler) as httpd:
-#     print("serving at port", PORT)
-#     httpd.serve_forever()
 
